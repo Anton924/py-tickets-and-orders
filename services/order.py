d6fdc8b292b5dee3c7c3f5fed92c59ced15788e7
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2025-08-30 00:39:57 +0200

    Correction from the checklist

--- services/order.py
+++ services/order.py
@@ -5,29 +5,28 @@ from django.db.models import QuerySet
 from db.models import Order, Ticket
 import datetime
 
-
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: datetime = None
 ) -> None:
-    with transaction.atomic():
-        user = get_user_model().objects.get(username=username)
-        order = Order.objects.create(user=user)
-        if date:
-            order.created_at = date
-            order.save()
-        for ticket in tickets:
-            row, seat, movie_session = ticket.values()
-            Ticket.objects.create(
-                row=row,
-                seat=seat,
-                movie_session_id=movie_session,
-                order=order
-            )
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = date
+        order.save()
+    for ticket in tickets:
+        row, seat, movie_session = ticket.values()
+        Ticket.objects.create(
+            row=row,
+            seat=seat,
+            movie_session_id=movie_session,
+            order=order
+        )
 
 
-def get_orders(username: str = None) -> QuerySet:
+def get_orders(username: str = None) -> QuerySet[Order]:
     queries = Order.objects.all()
     if username:
         queries = queries.filter(user__username=username)
